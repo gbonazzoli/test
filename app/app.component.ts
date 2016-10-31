fea9cd5cdf1b9dd235573bde9eba69bd743e0a2f
Gian Domenico Bonazzoli <gbonazzoli@Gians-iMac.local>
2016-10-31 08:56:49 +0100

    Primo lancio applicativo

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>My First Angular App</h1>'
+})
+// You export the AppComponent class so that you can import it into the application that you just created.
+export class AppComponent { }
